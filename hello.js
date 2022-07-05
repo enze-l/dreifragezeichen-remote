72472c4675ba8998715499a334db65b631bdb01f
enze-l <leon.enzenberger@protonmail.com>
2022-07-05 12:49:04 +0200

    Use express to manage login flow

--- hello.js
+++ hello.js
@@ -1,23 +1,33 @@
 const SpotifyWebApi = require('spotify-web-api-node')
+const express = require('express')
 const dotenv = require('dotenv')
+
+const app = express()
 dotenv.config()
 
-const scopes = [
-    'app-remote-control',
-    'streaming',
-    'user-modify-playback-state',
-    'user-read-playback-state',
-    'user-read-currently-playing'
-]
+app.get('/login', (req, res) => {
+    const scopes = [
+        'app-remote-control',
+        'streaming',
+        'user-modify-playback-state',
+        'user-read-playback-state',
+        'user-read-currently-playing'
+    ]
 
-const spotify = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: "http://localhost:631"
+    const spotify = new SpotifyWebApi({
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: "http://localhost:8083/login/success"
+    })
+
+    const authorizeURL = spotify.createAuthorizeURL(scopes, "hello");
+    res.redirect(authorizeURL)
 })
 
-const code = process.env.AUTH_CODE
+app.get('/login/success', (req, res) =>{
+    res.send("Login successful")
+    const code = req.query.code
+})
 
-const authorizeURL = spotify.createAuthorizeURL(scopes, "hello");
-
-console.log(authorizeURL)
+app.listen(8083)
+console.log("Server running")
