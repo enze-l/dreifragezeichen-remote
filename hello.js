f20fc58d51103fa167263f2855decca2e831f72d
enze-l <leon.enzenberger@protonmail.com>
2022-07-05 15:01:31 +0200

    Transfer playback to 'Wohnzimmer'

--- hello.js
+++ hello.js
@@ -5,6 +5,14 @@ const dotenv = require('dotenv')
 const app = express()
 dotenv.config()
 
+const login = () =>{
+    return new SpotifyWebApi({
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: "http://localhost:8083/login/success"
+    })
+}
+
 app.get('/login', (req, res) => {
     const scopes = [
         'app-remote-control',
@@ -14,19 +22,35 @@ app.get('/login', (req, res) => {
         'user-read-currently-playing'
     ]
 
-    const spotify = new SpotifyWebApi({
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        redirectUri: "http://localhost:8083/login/success"
-    })
+    const spotify = login()
 
     const authorizeURL = spotify.createAuthorizeURL(scopes, "hello");
+
     res.redirect(authorizeURL)
 })
 
 app.get('/login/success', (req, res) =>{
     res.send("Login successful")
     const code = req.query.code
+
+    const spotify = login()
+
+    spotify.authorizationCodeGrant(code).then(
+        async function (data) {
+            spotify.setAccessToken(data.body['access_token']);
+            spotify.setRefreshToken(data.body['refresh_token']);
+            spotify.getMyDevices().then((data) => {
+                data.body.devices.forEach(
+                    (device) => {
+                        console.log(device.is_active + " " + device.type + " " + device.name + ": " + device.id)
+                    }
+                )
+            })
+            await spotify.transferMyPlayback(['7769113f1c3db537e4e6827de2615c3799d70f17'], {play: true})
+        },
+        function(err) {
+            console.log('Something went wrong!', err);
+        })
 })
 
 app.listen(8083)
