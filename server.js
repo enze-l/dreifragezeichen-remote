9bc3eb4d00a311cda30c66f03a2f674bfb9f5697
enze-l <leon.enzenberger@protonmail.com>
2022-07-07 20:49:12 +0200

    Play random or latest album if 'playRandom' is called

--- server.js
+++ server.js
@@ -2,25 +2,47 @@ const express = require('express')
 const dotenv = require('dotenv')
 const axios = require('axios').default
 const spotify = require('spotify-finder')
+const fs = require('fs')
 
 const app = express()
 dotenv.config()
 
 const deviceUrl = 'http://192.168.2.31:24879'
+let settings = {
+    "currentAlbum": "3NYWEuwAPt4PIJ4OeNxmzO",
+    "lastAlbumCount": 261
+}
+
+function loadSettings() {
+    let rawData = fs.readFileSync('./savestate.json')
+    settings = JSON.parse(rawData)
+}
+
+function saveSettings() {
+    let data = JSON.stringify(settings, null, 2)
+    fs.writeFileSync('./savestate.json', data)
+}
 
 const post = async (res, path) => {
     const response = await axios.post(deviceUrl + path)
         .then(response => {
-            console.log(response)
+            //console.log(response)
         })
         .catch(error => {
-            console.log(error)
+            //console.log(error)
         })
     res.send(response)
 }
 
 app.post('/play-pause', async (req, res) =>{
-    await post(res,"/player/play-pause")
+    let track
+    await axios.post(deviceUrl + "/player/current").then(current =>{
+        track = current.data.track
+    })
+    if(track) {
+        await post(res, "/player/play-pause")
+    }
+    //todo play last played track
 })
 
 app.post('/next', async (req, res) => {
@@ -48,26 +70,38 @@ app.post('/playRandom', async (req, res) => {
         totalAlbumCount = albums.total
     })
 
-    const albumNumber = Math.floor(Math.random() * (totalAlbumCount - 1) + 1)
+    let albumId
+    if(totalAlbumCount > settings.lastAlbumCount) {
+        albumId = await getAlbumId(client, dreiFragezeichenId, totalAlbumCount)
+    } else {
+        const albumNumber = Math.floor(Math.random() * (totalAlbumCount - 1) + 1)
+        albumId = await getAlbumId(client, dreiFragezeichenId, albumNumber)
+    }
+    console.log(albumId)
 
+    await axios.post(deviceUrl + "/player/load", "uri=spotify:album:" + albumId + "&play=true&shuffle=false")
+    res.send()
+}, function (err) {
+    console.log('Something went wrong!', err);
+})
+
+const getAlbumId = async (client, artistID, albumNumber) =>{
     const offset = albumNumber - albumNumber % 50;
     const limit = 50;
     const adjustedAlbumNumber = albumNumber - offset;
-    let albumId;
-    client.getArtist(dreiFragezeichenId,{
+    let id
+    await client.getArtist(artistID,{
         albums: true,
         album_type: 'album',
         limit,
         offset
     }).then(albums =>{
-        albumId = albums.items[adjustedAlbumNumber].id
+        id = albums.items[adjustedAlbumNumber].id
     })
+    return id
+}
 
-    await axios.post(deviceUrl + "/player/load", "uri=spotify:album:3NYWEuwAPt4PIJ4OeNxmzO&play=true&shuffle=false")
-    res.send()
-}, function (err) {
-    console.log('Something went wrong!', err);
-})
+loadSettings()
 
 app.listen(8083)
 console.log("Server running")
